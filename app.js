c3d19738ca4044f95c4b933a172f9fca2a0006cd
Ajeet Shah <ajeets.shah@hotmail.com>
2019-04-18 01:53:18 +0530

    Add users, books and tasks

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index')
+let usersRouter = require('./routes/users')
+let booksRouter = require('./routes/books')
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,11 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/books', booksRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

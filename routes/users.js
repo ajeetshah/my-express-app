1c82641eff9ae52274b0324b87c098a82823f549
Ajeet Shah <ajeets.shah@hotmail.com>
2019-04-19 00:53:24 +0530

    Add mysql

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const mysql = require('mysql')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource')
-});
+})
+
+router.get('/db', function(req, res, next) {
+  
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'rootuser',
+    password : 'Pass@1234',
+    database : 'mysql'
+  });
+
+  connection.connect()
+
+  connection.query('SELECT User, Host FROM mysql.user;', function (err, rows, fields) {
+    if (err) throw err
+    console.log('query result is: ', rows)
+  })
+
+  connection.end()
+
+  res.send('i will test db')
+})
 
 module.exports = router
